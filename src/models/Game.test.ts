73937237318ca407e09d73374c7dd6cbbe7f6912
skarahoda <skarahoda@sabanciuniv.edu>
2019-12-20 15:58:15 +0300

    Adds players

--- src/models/Game.test.ts
+++ src/models/Game.test.ts
@@ -1,7 +1,13 @@
-import { Game } from './Game';
+import {Game} from './Game';
+import {Player} from './Player';
 
 describe('Game', () => {
     it('should create object', () => {
         expect(new Game()).toBeDefined();
+    });
+    it('should have two player', () => {
+        const game = new Game();
+        expect(game.getPlayer1()).toBeInstanceOf(Player);
+        expect(game.getPlayer2()).toBeInstanceOf(Player);
     });
 });
--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,2 +1,19 @@
+import {Player} from './Player';
+
 export class Game {
+    private readonly player1;
+    private readonly player2;
+
+    constructor() {
+        this.player1 = new Player();
+        this.player2 = new Player();
+    }
+
+    getPlayer1() {
+        return this.player1;
+    }
+
+    getPlayer2() {
+        return this.player2;
+    }
 }
